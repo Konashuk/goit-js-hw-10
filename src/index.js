89c45f7822b044adf987968ee195210815e88d2d
Konashuk <konan4ukv@gmail.com>
2023-09-25 14:33:11 +0300

    add all mukkap

--- src/index.js
+++ src/index.js
@@ -1,21 +1,32 @@
 import axios from 'axios';
-import { fetchBreeds, fetchCatByBreed } from './js/cat-api';
 import SlimSelect from 'slim-select';
+
+import { fetchBreeds, fetchCatByBreed } from './js/cat-api';
 
 axios.defaults.headers.common['x-api-key'] =
   'live_EBFK47rnbJbxqGyEs0tu4iC9ZJQc9CkpcCB6F4BWv2qM2Q3vddCcoLIukgwT26zy';
 
-const error = document.querySelector('.error');
+const errorEl = document.querySelector('.error');
 const selectBeeds = document.querySelector('.breed-select');
 const containerCat = document.querySelector('.cat-info');
+const loaderEl = document.querySelector('.loader');
 
 selectBeeds.addEventListener('change', clickedSelect);
+errorEl.classList.add('is-hidden');
+selectBeeds.classList.add('is-hidden');
 
 fetchBreeds()
   .then(data => selectOption(data))
-  .catch(err => console.log(err));
+  .catch(err => errorMess());
+
+function errorMess() {
+  errorEl.classList.remove('is-hidden');
+  loaderEl.classList.add('is-hidden');
+}
 
 function selectOption(data) {
+  selectBeeds.classList.remove('is-hidden');
+  loaderEl.classList.add('is-hidden');
   const murkapSelect = data
     .map(({ id, name }) => {
       return `<option value="${id}">${name}</option>`;
@@ -23,27 +34,31 @@ function selectOption(data) {
     .join('');
 
   selectBeeds.insertAdjacentHTML('afterbegin', murkapSelect);
-
   new SlimSelect({
-    select: '#selectElement',
+    select: selectBeeds,
+    settings: {
+      placeholderText: "Here is Cat's breed",
+    },
   });
 }
 
 function clickedSelect(event) {
   const breedId = event.currentTarget.value;
+  loaderEl.classList.remove('is-hidden');
   fetchCatByBreed(breedId)
     .then(data => cartCarMurkap(data))
-    .catch(err => console.log(err));
+    .catch(err => console.log(errorEl));
 }
-
 function cartCarMurkap(data) {
+  loaderEl.classList.add('is-hidden');
+  containerCat.classList.add('.is-hidden');
   const nameBreed = data[0].breeds[0].name;
   const descBreed = data[0].breeds[0].description;
   const temperBred = data[0].breeds[0].temperament;
   const breedImg = data[0].url;
 
   const cardCatMurkup = `
-  <img src="${breedImg}" width="300" alt="${nameBreed}"/>
+  <img src="${breedImg}" width="400" height="300"alt="${nameBreed}"/>
  <div claas="container-text">
   <h2>${nameBreed}</h2>
   <p>${descBreed}</p>
--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -16,7 +16,7 @@ export function fetchCatByBreed(breedId) {
     'api_key=live_EBFK47rnbJbxqGyEs0tu4iC9ZJQc9CkpcCB6F4BWv2qM2Q3vddCcoLIukgwT26zy';
   return fetch(`${URL_SERCH}${PART_URL_DREED_ID}&${Api_KEY}`).then(res => {
     if (!res.ok) {
-      throw new Error(res.statusText);
+      throw new Error(errorEl.claslist.remore('is-hidden'));
     }
     return res.json();
   });
